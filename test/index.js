656f75c82c2e7f7223aed34f7af9b043233f556b
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-12-07 13:13:31 +0600

    Initial Commit

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-import "es5-shim";
-import "es6-shim";
+require("es5-shim");
+require("es6-shim");
 const testsContext = require.context(".", true, /-spec$/);
 testsContext.keys().forEach(testsContext);
